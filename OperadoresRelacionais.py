e1cdcf593c034df19f2719cae2385b5c9d1d9da4
NeroGrande <anterotarquinio1234@gmail.com>
2024-10-16 21:43:37 -0300

    Add files via upload

---
+++ OperadoresRelacionais.py
@@ -0,0 +1,19 @@
+"LENDA VIVA GRANDE NERO"
+"-------------------------------"
+#MAIOR QUE:
+print(2>3)
+
+#MENOR QUE:
+print(2<3)
+
+#MENOR E IGUAL:
+print(2<=3)
+
+#IGUAL
+print(3==3)
+
+#DIFERENTE
+print(3!=4)
+
+#MAIOR OU IGUAL:
+print(2>=3)
